99a8c702a7da703a6c294d77d57de9e6b9ca7f97
Daniel Medeiros <dmedeiros@mobitv.com>
2019-11-11 16:38:07 -0800

    support configuration of uninterruptable runner executor

--- blocker-jvm/src/main/kotlin/com/dmedeiros/blocker/jvm/BlockerJvm.kt
+++ blocker-jvm/src/main/kotlin/com/dmedeiros/blocker/jvm/BlockerJvm.kt
@@ -1,6 +1,7 @@
 package com.dmedeiros.blocker.jvm
 
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.TimeUnit.MILLISECONDS
@@ -56,7 +57,7 @@ inline fun <reified T, R> (((T) -> Unit) -> R).blockUntilCallback(): T {
  *
  * @param timeoutMs how long to wait for the callback to be invoked.
  *
- * @return true if the method finished executing withing [timeoutMs] milliseconds
+ * @return true if the method finished executing within [timeoutMs] milliseconds
  *
  * @throws InterruptedException if the blocking thread is interrupted
  */
@@ -70,8 +71,8 @@ fun ((() -> Unit) -> Unit).runNoResultBlocking(timeoutMs: Long = 500): Boolean {
 }
 
 /**
- * runs a block on another thread, uninterruptibly blocking the current thread and returning the
- * result of the block execution.
+ * uninterruptibly runs a block on a default thread pool and blocks until the result of the block execution is
+ * available.
  *
  * this function is for executing critical tasks that must be completed before the running thread
  * may be interrupted. since the current thread will be blocked, be aware of any deadlocks that
@@ -80,23 +81,36 @@ fun ((() -> Unit) -> Unit).runNoResultBlocking(timeoutMs: Long = 500): Boolean {
  * if the current thread is interrupted by the time this function returns, the thread's interrupted
  * flag will be set. it is the caller's responsibility to throw interrupted exception if this
  * flag is set.
- *
  */
-fun <R> runUninterruptibly(block: () -> R): R {
-    val lock = ReentrantLock()
-    val blockFinishedCondition = lock.newCondition()
+fun <R> runUninterruptibly(block: () -> R): R =
+    UninterruptableRunner(uninterruptableThreadPoolExecutor.value).run(block)
 
-    lock.withLock {
-        val resultContainer: MutableSet<R> = mutableSetOf()
-        uninterruptableThreadPoolExecutor.value.execute {
-            lock.withLock {
-                val result = block()
-                resultContainer += result
-                blockFinishedCondition.signal()
+/**
+ * returns a DSL to configure an executor on which to run a block uninterruptibly.
+ */
+fun <R> runUninterruptibly(): UninterruptableRunnerConfigure = UninterruptableRunnerConfigure()
+
+class UninterruptableRunnerConfigure internal constructor() {
+    fun <R> onExecutor(executor: Executor, block: () -> R): R = UninterruptableRunner(executor).run(block)
+}
+
+private class UninterruptableRunner constructor(private val executor: Executor) {
+    private val lock = ReentrantLock()
+    private val blockFinishedCondition = lock.newCondition()
+
+    internal fun <R> run(block: () -> R): R {
+        lock.withLock {
+            val resultContainer: MutableSet<R> = mutableSetOf()
+            executor.execute {
+                lock.withLock {
+                    val result = block()
+                    resultContainer += result
+                    blockFinishedCondition.signal()
+                }
             }
+            blockFinishedCondition.awaitUninterruptibly()
+            return resultContainer.single()
         }
-        blockFinishedCondition.awaitUninterruptibly()
-        return resultContainer.single()
     }
 }
 
