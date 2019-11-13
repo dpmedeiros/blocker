039fa64d2242b51acc0bcf427b7e58bf7728e09a
Daniel Medeiros <dmedeiros@mobitv.com>
2019-11-13 12:11:59 -0800

    remove unneccessary method

--- blocker-coroutines/src/main/kotlin/com/dmedeiros/blocker/coroutine/CoroutineSupport.kt
+++ blocker-coroutines/src/main/kotlin/com/dmedeiros/blocker/coroutine/CoroutineSupport.kt
@@ -1,9 +1,5 @@
 package com.dmedeiros.blocker.coroutine
 
-import com.dmedeiros.blocker.jvm.blockUntilCallback
-import kotlinx.coroutines.CompletionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -18,7 +14,7 @@ import kotlin.coroutines.suspendCoroutine
  *
  */
 @Suppress("TooGenericExceptionCaught")
-suspend fun <T, E : Throwable, R> coroutineBridge(f: ((T) -> Unit, (E) -> Unit) -> R): T =
+suspend inline fun <T, E : Throwable, R> coroutineBridge(crossinline f: ((T) -> Unit, (E) -> Unit) -> R): T =
     suspendCoroutine { continuation: Continuation<T> ->
         try {
             f({
@@ -30,17 +26,3 @@ suspend fun <T, E : Throwable, R> coroutineBridge(f: ((T) -> Unit, (E) -> Unit) 
             continuation.resumeWithException(throwable)
         }
     }
-
-/**
- * starts a coroutine in the given scope from outside of a coroutine, and blocks until the coroutine returns a result
- */
-fun <T> CoroutineScope.awaitCoroutineResult(f: suspend () -> T): T {
-    val job = async { f() }
-    val invokeOnCompletion = fun(handler: CompletionHandler) = job.invokeOnCompletion(handler)
-
-    val error = invokeOnCompletion.blockUntilCallback()
-    error?.also { throw it }
-
-    @Suppress("EXPERIMENTAL_API_USAGE")
-    return job.getCompleted()
-}
